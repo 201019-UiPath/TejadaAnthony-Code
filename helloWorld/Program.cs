f702926e9824a1b93da1815c9cb4274241c1473f
Anthony <tejant96@gmail.com>
2020-10-19 22:01:08 -0400

    folder fix

---
+++ helloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string name;
+            Console.WriteLine("Enter Name: ");
+            
+            name = Console.ReadLine();
+
+            Console.WriteLine($"Hello there {name}");
+        }
+    }
+}
